c95c7c340bca0c1b3a04204da7e49f086483af20
RosaryAbilash <75167373+RosaryAbilash@users.noreply.github.com>
2021-06-09 06:44:12 +0530

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,30 @@
+word = "secret"
+
+guesses = []
+errors = 7
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print("")
+
+    guess = input(f"Allowed Error Left {errors}, Next Guess: ")
+    guesses.append(guess.lower())
+    if guess.lower() not in word.lower():
+        errors -= 1
+        if errors == 0:
+            break
+
+    done = True
+    for letter in word:
+        if letter.lower() not in guesses:
+            done = False
+
+if done:
+    print(f"You found the word. It was {word}")
+else:
+    print(f"Game Over. The word was {word}")
